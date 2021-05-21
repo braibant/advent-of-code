feca1ec161202e70e03fed40b592be2078dc4934
Thomas Braibant <thomas.braibant@gmail.com>
2021-05-21 12:57:07 +0100

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,146 @@
 use std::env;
-use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::process;
 
 enum Problem {
     Day1(String),
+    Day2(String),
+}
+
+// Assumes that [v] is sorted
+fn sum2(v: &[i32], tgt: i32) -> Option<(usize, usize)> {
+    let mut i = 0;
+    let mut j = v.len() - 1;
+    while i < j {
+        if v[i] + v[j] < tgt {
+            i += 1
+        } else if v[i] + v[j] > tgt {
+            j -= 1
+        } else {
+            assert_eq!(v[i] + v[j], tgt);
+            return Some((i, j));
+        }
+    }
+    return None;
+}
+
+// Assumes that [v] is sorted
+fn sum3(v: &[i32], tgt: i32) -> Option<(usize, usize, usize)> {
+    for i in 0..v.len() {
+        match sum2(&v[i + 1..], tgt - v[i]) {
+            None => (),
+            Some((j, k)) => {
+                return Some((i, j + i + 1, k + i + 1));
+            }
+        }
+    }
+    return None;
 }
 
 fn day1(filename: String) {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
     let mut content = Vec::new();
-    for (index, line) in reader.lines().enumerate() {
+    for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
         let num: i32 = line.parse().unwrap();
         content.push(num);
     }
 
     content.sort();
+    let (i, j) = sum2(&content, 2020).unwrap();
+    println!("sum2:{}", content[i] * content[j]);
 
-    let mut i = 0;
-    let mut j = content.len() - 1;
-    while i < j {
-        if (content[i] + content[j] < 2020) {
-            i += 1
-        } else if (content[i] + content[j] > 2020) {
-            j -= 1
+    let (i, j, k) = sum3(&content, 2020).unwrap();
+    println!(
+        "sum3:{} {} {} {}",
+        content[i],
+        content[j],
+        content[k],
+        content[i] * content[j] * content[k]
+    );
+}
+
+#[derive(Debug)]
+struct Password {
+    low: i32,
+    high: i32,
+    letter: char,
+    password: String,
+}
+
+impl Password {
+    fn parse(s: String) -> Option<Password> {
+        let v: Vec<&str> = s.split(&['-', ':', ' '][..]).collect();
+        if v.len() != 5 {
+            return None;
         } else {
-            println!("{}", content[i] * content[j]);
-            return;
+            let low: i32 = v[0].parse().unwrap();
+            let high: i32 = v[1].parse().unwrap();
+            let letter: char = v[2].parse().unwrap();
+            let password: String = v[4].parse().unwrap();
+            Some(Password {
+                low,
+                high,
+                letter,
+                password,
+            })
         }
     }
 
-    panic!("Invariant does not hold")
+    fn check1(&self) -> bool {
+        fn count(x: char, s: &str) -> i32 {
+            let mut i = 0;
+            for c in s.chars() {
+                if c == x {
+                    i += 1
+                }
+            }
+            i
+        }
+        let n = count(self.letter, &self.password);
+        self.low <= n && n <= self.high
+    }
+
+    fn check2(&self) -> bool {
+        let chars: Vec<char> = self.password.chars().collect();
+        let p1 = chars[(self.low - 1) as usize] == self.letter;
+        let p2 = chars[(self.high - 1) as usize] == self.letter;
+        !(p1 && p2) && (p1 || p2)
+    }
 }
 
+fn day2(filename: String) {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut check1 = 0;
+    let mut check2 = 0;
+    for (_index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        let password = Password::parse(line).unwrap();
+        if Password::check1(&password) {
+            check1 += 1
+        };
+        if Password::check2(&password) {
+            check2 += 1
+        }
+    }
+    println!("check1 {}", check1);
+    println!("check2 {}", check2)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn day2_example() {
+        let password = Password::parse("1-3 a: abcde".to_string()).unwrap();
+        assert!(Password::check(&password));
+        let password = Password::parse("1-3 b: cdefg".to_string()).unwrap();
+        assert!(!(Password::check(&password)))
+    }
+}
 impl Problem {
     fn new(args: &[String]) -> Result<Problem, &str> {
         if args.len() < 2 {
@@ -48,6 +153,12 @@ impl Problem {
                     return Err("not enough arguments");
                 };
                 return Ok(Problem::Day1(args[2].clone()));
+            }
+            "2" => {
+                if args.len() < 3 {
+                    return Err("not enough arguments");
+                };
+                return Ok(Problem::Day2(args[2].clone()));
             }
             _ => return Err("Problem not yet implemented"),
         }
@@ -62,5 +173,6 @@ fn main() {
     });
     match problem {
         Problem::Day1(filename) => day1(filename),
+        Problem::Day2(filename) => day2(filename),
     }
 }
