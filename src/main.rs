e3f17b6bac320278768069a5ff790df0bcfeca79
Thomas Braibant <thomas.braibant@gmail.com>
2021-05-25 14:45:54 +0100

    Day 9

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
 use std::env;
-use std::process;
 
-enum Problem {
-    Day1(String),
-    Day2(String),
-    Day3(String),
-    Day4(String),
-    Day5(String),
-    Day6(String),
-    Day7(String),
-    Day8(String),
-}
 mod day1;
 mod day2;
 mod day3;
@@ -19,57 +8,21 @@ mod day5;
 mod day6;
 mod day7;
 mod day8;
-
-impl Problem {
-    fn new(args: &[String]) -> Result<Problem, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        };
-        let day = args[1].clone();
-        match day.as_str() {
-            "1" => {
-                return Ok(Problem::Day1(args[2].clone()));
-            }
-            "2" => {
-                return Ok(Problem::Day2(args[2].clone()));
-            }
-            "3" => {
-                return Ok(Problem::Day3(args[2].clone()));
-            }
-            "4" => {
-                return Ok(Problem::Day4(args[2].clone()));
-            }
-            "5" => {
-                return Ok(Problem::Day5(args[2].clone()));
-            }
-            "6" => {
-                return Ok(Problem::Day6(args[2].clone()));
-            }
-            "7" => {
-                return Ok(Problem::Day7(args[2].clone()));
-            }
-            "8" => {
-                return Ok(Problem::Day8(args[2].clone()));
-            }
-            _ => return Err("Problem not yet implemented"),
-        }
-    }
-}
+mod day9;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let problem = Problem::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
-    match problem {
-        Problem::Day1(filename) => day1::run(filename),
-        Problem::Day2(filename) => day2::run(filename),
-        Problem::Day3(filename) => day3::run(filename),
-        Problem::Day4(filename) => day4::run(filename),
-        Problem::Day5(filename) => day5::run(filename),
-        Problem::Day6(filename) => day6::run(filename),
-        Problem::Day7(filename) => day7::run(filename),
-        Problem::Day8(filename) => day8::run(filename),
+    let filename = args[2].clone();
+    match args[1].as_str() {
+        "1" => day1::run(filename),
+        "2" => day2::run(filename),
+        "3" => day3::run(filename),
+        "4" => day4::run(filename),
+        "5" => day5::run(filename),
+        "6" => day6::run(filename),
+        "7" => day7::run(filename),
+        "8" => day8::run(filename),
+        "9" => day9::run(filename),
+        _ => panic!("Not yet implemented"),
     }
 }
