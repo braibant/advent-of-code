1e3c6e987dba133ebb8534bcf6af8f409b38f398
Thomas Braibant <thomas.braibant@gmail.com>
2021-05-27 09:29:08 +0100

    Day 15

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ mod day11;
 mod day12;
 mod day13;
 mod day14;
+mod day15;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -37,6 +38,7 @@ fn main() {
         "12" => day12::run(filename),
         "13" => day13::run(filename),
         "14" => day14::run(filename),
+        "15" => day15::run(),
         _ => panic!("Not yet implemented"),
     }
 }
