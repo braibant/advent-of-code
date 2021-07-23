f0193de6f7f26a72c9e7abc2346979d0e95cf1bd
Thomas Braibant <thomas.braibant@gmail.com>
2021-07-23 17:58:46 +0200

    Day 12, part 2

--- src/day_12.rs
+++ src/day_12.rs
@@ -24,6 +24,19 @@ impl<T> Vector3<T> {
     }
 }
 
+use std::ops::Index;
+impl<T> Index<usize> for Vector3<T> {
+    type Output = T;
+    fn index(&self, index: usize) -> &T {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            _ => panic!("Invalid index"),
+        }
+    }
+}
+
 #[derive(Debug)]
 struct T {
     position: Vector3<i64>,
@@ -101,6 +114,71 @@ fn total_energy(state: &[T]) -> i64 {
     state.iter().map(|t| energy(t)).sum()
 }
 
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+struct T1 {
+    position: i64,
+    velocity: i64,
+}
+
+fn step_1d(state: &[T1; 4]) -> [T1; 4] {
+    let mut next = *state;
+    for i in 0..4 {
+        for j in 0..4 {
+            if i != j {
+                if state[i].position < state[j].position {
+                    next[i].velocity += 1
+                };
+                if state[i].position > state[j].position {
+                    next[i].velocity -= 1
+                };
+            }
+        }
+    }
+    for i in 0..4 {
+        next[i].position += next[i].velocity
+    }
+    next
+}
+
+// Returns (n,m) where m is the index of the first state that matches a previous state (n)
+fn find_period_1d(state: &[T1; 4]) -> (usize, usize) {
+    use std::collections::HashMap;
+
+    let mut state = *state;
+    let mut i: usize = 0;
+    let mut seen = HashMap::new();
+    loop {
+        match seen.get(&state) {
+            None => seen.insert(state, i),
+            Some(&previous) => return (previous, i),
+        };
+        state = step_1d(&state);
+        i += 1;
+    }
+}
+
+fn project(state: &[T], dimension: usize) -> [T1; 4] {
+    assert!(state.len() == 4);
+
+    let mut state_1d = [T1 {
+        position: 0,
+        velocity: 0,
+    }; 4];
+    for i in 0..4 {
+        state_1d[i].position = state[i].position[dimension];
+        state_1d[i].velocity = state[i].velocity[dimension];
+    }
+    state_1d
+}
+
+fn find_period_3d(state: &[T]) -> Vec<(usize, usize)> {
+    let mut periods = vec![];
+    for dimension in 0..3 {
+        periods.push(find_period_1d(&project(state, dimension)))
+    }
+    periods
+}
+
 pub fn run() {
     let positions: Vec<Vector3<i64>> = vec![
         Vector3::new(13, -13, -2),
@@ -111,7 +189,20 @@ pub fn run() {
 
     let state = simulate(&positions, 1000);
 
-    println!("{}", total_energy(&state))
+    println!("{}", total_energy(&state));
+
+    // Part 2: we can make the observation that the accelation depends only on
+    // the ordering on 3 axis of the 4 bodies. Also, there is no interaction
+    // between the three axis: we could compute the periodicity on each axis,
+    // and find the lowest common multiple of the periodicity on each axis.
+
+    let periods = find_period_3d(&state);
+
+    // It turns out that the first repeating state on a given axis is the
+    // projection of the initial state along this axis. This simplifies the
+    // problem quite a bit, since we "just" need to compute the lcm of the
+    // periods involved to figure out the period of the whole system. Here, I simply used wolfram alpha to compute the lcm
+    println!("{:?}", periods);
 }
 
 #[cfg(test)]
