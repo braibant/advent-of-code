658d98b6efc8bb6aa6e0c8770e46d22a1e3498dc
Thomas Braibant <thomas.braibant@gmail.com>
2021-08-10 15:57:03 +0200

    Day 20, part 2

--- src/day_20.rs
+++ src/day_20.rs
@@ -5,14 +5,17 @@ use std::collections::HashSet;
 
 type Pos = Vector2<i32>;
 
+#[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
+enum Side {
+    Outer,
+    Inner,
+}
+
 #[derive(Debug)]
 struct Parsed {
     tiles: HashSet<Pos>,
-    warps: Vec<(Pos, String)>,
-}
-
-struct T {
-    neighbours: HashMap<Pos, Vec<Pos>>,
+    warps_pl: HashMap<Pos, (String, Side)>,
+    warps_lp: HashMap<String, Vec<(Pos, Side)>>,
 }
 
 lazy_static! {
@@ -22,47 +25,62 @@ lazy_static! {
     };
 }
 
-fn warp_pos(warps: &[(Pos, String)], label: &str) -> Vec<Pos> {
-    warps
-        .iter()
-        .filter_map(|(p, l)| if l == label { Some(*p) } else { None })
-        .collect()
+fn warp_destination(parsed: &Parsed, label: &str, src: Pos) -> Option<(Pos, Side)> {
+    parsed
+        .warps_lp
+        .get(label)
+        .unwrap_or(&vec![])
+        .into_iter()
+        .find_map(|(p, s)| if *p != src { Some((*p, *s)) } else { None })
 }
 
-fn warp_destination(warps: &[(Pos, String)], label: &str, src: Pos) -> Option<Pos> {
-    warps.iter().find_map(|(p, l)| {
-        if *p != src && l == label {
-            Some(*p)
-        } else {
-            None
+fn neighbours(parsed: &Parsed, pos: Pos) -> Vec<Pos> {
+    let mut acc = vec![];
+    for &d in DIRS.iter() {
+        if parsed.tiles.contains(&(pos + d)) {
+            acc.push(pos + d);
         }
-    })
-}
-
-fn build(parsed: &Parsed) -> T {
-    let warps_pl: HashMap<Pos, String> = parsed.warps.iter().cloned().collect();
-    let mut neighbours = HashMap::new();
-    let todo: Vec<_> = parsed.tiles.iter().collect();
-    for &tile in parsed.tiles.iter() {
-        let mut acc = vec![];
-        for &d in DIRS.iter() {
-            if parsed.tiles.contains(&(tile + d)) {
-                acc.push(tile + d);
-            }
-        }
-        match warps_pl.get(&tile) {
-            None => {}
-            Some(n) => match warp_destination(&parsed.warps, n, tile) {
-                None => {}
-                Some(d) => acc.push(d),
-            },
-        };
-        neighbours.insert(tile, acc);
     }
-    T { neighbours }
+    match parsed.warps_pl.get(&pos) {
+        None => {}
+        Some((n, _side)) => match warp_destination(&parsed, n, pos) {
+            None => {}
+            Some((d, _s)) => acc.push(d),
+        },
+    };
+    acc
 }
 
-fn bfs(t: &T, src: Pos, dst: Pos) -> Option<u32> {
+fn neighbours_rec(parsed: &Parsed, (pos, level): (Pos, u32)) -> Vec<(Pos, u32)> {
+    let mut acc = vec![];
+    for &d in DIRS.iter() {
+        if parsed.tiles.contains(&(pos + d)) {
+            acc.push((pos + d, level));
+        }
+    }
+    match parsed.warps_pl.get(&pos) {
+        None => {}
+        Some((n, side)) => match warp_destination(&parsed, n, pos) {
+            None => {}
+            Some((d, dside)) => {
+                if *side == dside {
+                    acc.push((d, level))
+                } else if *side == Side::Outer && 0 < level {
+                    acc.push((d, level - 1))
+                } else if *side == Side::Inner && level < 500 {
+                    acc.push((d, level + 1))
+                }
+            }
+        },
+    };
+    acc
+}
+
+fn bfs<T, F>(neighbours: F, src: T, dst: T) -> Option<u32>
+where
+    F: Fn(T) -> Vec<T>,
+    T: Eq + std::hash::Hash + Copy,
+{
     let mut todo = std::collections::VecDeque::new();
     let mut visited = HashSet::new();
     todo.push_back((src, 0));
@@ -73,13 +91,13 @@ fn bfs(t: &T, src: Pos, dst: Pos) -> Option<u32> {
             if next == dst {
                 return Some(dist);
             } else {
-                for n in t.neighbours.get(&next).unwrap_or(&vec![]).iter() {
+                for n in neighbours(next).iter() {
                     todo.push_back((*n, dist + 1))
                 }
             }
         }
     }
-    return None;
+    None
 }
 
 // x ->
@@ -93,20 +111,29 @@ fn parse(content: &str) -> Parsed {
     }
     let height = image.len();
     let width = image[0].len();
-    let mut warps: Vec<(Pos, String)> = vec![];
+    let mut warps: Vec<(Pos, String, Side)> = vec![];
     for y in 0..height {
         for x in 0..(width - 2) {
             let a = image[y][x];
             let b = image[y][x + 1];
             let c = image[y][x + 2];
-
+            let side = if x == 0 || x == width - 3 {
+                Side::Outer
+            } else {
+                Side::Inner
+            };
             if a.is_alphabetic() && b.is_alphabetic() && c == '.' {
                 warps.push((
                     Vector2::new((x + 2) as i32, y as i32),
                     format!("{}{}", a, b),
+                    side,
                 ))
             } else if a == '.' && b.is_alphabetic() && c.is_alphabetic() {
-                warps.push((Vector2::new(x as i32, y as i32), format!("{}{}", b, c)))
+                warps.push((
+                    Vector2::new(x as i32, y as i32),
+                    format!("{}{}", b, c),
+                    side,
+                ))
             }
         }
     }
@@ -115,13 +142,23 @@ fn parse(content: &str) -> Parsed {
             let a = image[y][x];
             let b = image[y + 1][x];
             let c = image[y + 2][x];
+            let side = if y == 0 || y == height - 3 {
+                Side::Outer
+            } else {
+                Side::Inner
+            };
             if a.is_alphabetic() && b.is_alphabetic() && c == '.' {
                 warps.push((
                     Vector2::new(x as i32, (y + 2) as i32),
                     format!("{}{}", a, b),
+                    side,
                 ))
             } else if a == '.' && b.is_alphabetic() && c.is_alphabetic() {
-                warps.push((Vector2::new(x as i32, y as i32), format!("{}{}", b, c)))
+                warps.push((
+                    Vector2::new(x as i32, y as i32),
+                    format!("{}{}", b, c),
+                    side,
+                ))
             }
         }
     }
@@ -133,7 +170,23 @@ fn parse(content: &str) -> Parsed {
             }
         }
     }
-    Parsed { tiles, warps }
+    let warps_pl: HashMap<Pos, (String, Side)> = warps
+        .iter()
+        .cloned()
+        .map(|(pos, lbl, side)| (pos, (lbl, side)))
+        .collect();
+    let mut warps_lp = HashMap::new();
+    for (pos, label, side) in warps.iter() {
+        warps_lp
+            .entry(label.clone())
+            .or_insert(vec![])
+            .push((*pos, *side))
+    }
+    Parsed {
+        tiles,
+        warps_pl,
+        warps_lp,
+    }
 }
 pub fn run(filename: &str) {
     // Parsing here is a bit more complex than previous problems. Let's do this
@@ -144,8 +197,12 @@ pub fn run(filename: &str) {
 
     let contents = std::fs::read_to_string(filename).unwrap();
     let parsed = parse(&contents);
-    let t = build(&parsed);
-    let src = warp_pos(&parsed.warps, "AA")[0];
-    let tgt = warp_pos(&parsed.warps, "ZZ")[0];
-    println!("{:?}", bfs(&t, src, tgt))
+    // let t = build(&parsed);
+    let src = parsed.warps_lp.get("AA").unwrap()[0].0;
+    let tgt = parsed.warps_lp.get("ZZ").unwrap()[0].0;
+    println!("{:?}", bfs(|pos| neighbours(&parsed, pos), src, tgt));
+    println!(
+        "{:?}",
+        bfs(|pos| neighbours_rec(&parsed, pos), (src, 0), (tgt, 0))
+    );
 }
