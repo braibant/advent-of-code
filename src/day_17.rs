ac534040fd5a864e18d642da29716ea5cf1c6c36
Thomas Braibant <thomas.braibant@gmail.com>
2021-11-07 22:31:33 +0000

    Day 17, part 1 & 2

--- src/day_17.rs
+++ src/day_17.rs
@@ -4,10 +4,11 @@ use std::collections::HashMap;
 
 type Position = Vector2<i32>;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum Type {
     Clay,
     Water,
+    Transient,
 }
 
 #[derive(Debug, Clone)]
@@ -72,6 +73,7 @@ fn print(t: &T) {
                 None => b.push('.'),
                 Some(Type::Clay) => b.push('#'),
                 Some(Type::Water) => b.push('~'),
+                Some(Type::Transient) => b.push('|'),
             }
         }
         b.push('\n');
@@ -83,25 +85,22 @@ const D: Vector2<i32> = Vector2 { x: 0, y: 1 };
 const L: Vector2<i32> = Vector2 { x: -1, y: 0 };
 const R: Vector2<i32> = Vector2 { x: 1, y: 0 };
 
-// The goal of part 1 is to figure out how many tile the water can reach,
-// knowing that there is a spring at x=500, y=0. Water flows down, until it
-// reaches a level of clay. What happens next depends on the topology of the
-// clay. Let's look left and right (while there is clay under the level we
-// explore), until we reach a gap, or until we reach clay boundaries. If we
-// reach clay boundaries, mark this level as filled with water and iterate. If
-// we reach a gap on a single side, mark the tile "after the ledge" as a spring.
-// If we reach a gap on both sides, mark the tiles "after the ledges" as
-// springs. Since we have an infinite supply of water, we can just iterate from
-// the springs that were created at the previous step. Each spring will create
-// at most two other springs.
-fn down(t: &T, mut pos: Position) -> Option<Position> {
-    loop {
-        if pos.y > t.bottom_right.y {
-            return None;
-        } else if t.scan.get(&(pos + D)).is_some() {
-            return Some(pos);
-        } else {
-            pos = pos + D
+impl T {
+    fn solid(&self, pos: Position) -> bool {
+        matches!(self.scan.get(&(pos)), Some(Type::Water) | Some(Type::Clay))
+    }
+
+    fn transient(&self, pos: Position) -> bool {
+        matches!(self.scan.get(&(pos)), Some(Type::Transient))
+    }
+
+    fn insert(&mut self, pos: Position, ty: Type) {
+        self.scan.insert(pos, ty);
+        if pos.x < self.upper_left.x {
+            self.upper_left.x = pos.x
+        };
+        if self.bottom_right.x < pos.x {
+            self.bottom_right.x = pos.x
         }
     }
 }
@@ -115,9 +114,9 @@ enum Border {
 
 fn left(t: &T, mut pos: Position) -> Border {
     loop {
-        if t.scan.get(&(pos + D)).is_none() {
+        if !t.solid(pos + D) {
             return Border::Hole(pos);
-        } else if t.scan.get(&(pos + L)).is_some() {
+        } else if t.solid(pos + L) {
             return Border::Wall(pos + L);
         } else {
             pos = pos + L;
@@ -127,9 +126,9 @@ fn left(t: &T, mut pos: Position) -> Border {
 
 fn right(t: &T, mut pos: Position) -> Border {
     loop {
-        if t.scan.get(&(pos + D)).is_none() {
+        if !t.solid(pos + D) {
             return Border::Hole(pos);
-        } else if t.scan.get(&(pos + R)).is_some() {
+        } else if t.solid(pos + R) {
             return Border::Wall(pos + R);
         } else {
             pos = pos + R;
@@ -137,59 +136,120 @@ fn right(t: &T, mut pos: Position) -> Border {
     }
 }
 
-use std::collections::HashSet;
-fn pour(t: &mut T, p: Position) {
+fn pour(t: &mut T, spring: Position) {
     use Border::{Hole, Wall};
 
-    let mut progress = true;
+    t.insert(spring, Type::Transient);
 
-    while progress {
-        let mut todo = vec![p];
-        let mut visited = HashSet::new();
-        progress = false;
-        while let Some(p) = todo.pop() {
-            // println!("{:?}", p);
-            // print(&t);
-            if t.scan.get(&p).is_none() && p.y <= t.bottom_right.y && !visited.contains(&p) {
-                visited.insert(p);
-                match down(t, p) {
-                    None => {}
-                    Some(p1) => match (left(t, p1), right(t, p1)) {
-                        (Wall(l), Wall(r)) => {
-                            // println!("{:?} -> W{:?}W", p, p1);
-                            assert!(l.y == r.y);
-                            progress = true;
-                            for x in (l.x + 1)..r.x {
-                                t.scan.insert(Vector2::new(x, l.y), Type::Water);
-                            }
-                            // todo.push(p)
-                        }
-                        (Hole(l), Wall(_)) => todo.push(l),
-                        (Wall(_), Hole(r)) => {
-                            todo.push(r);
-                        }
-                        (Hole(l), Hole(r)) => {
-                            todo.push(l);
-                            todo.push(r)
-                        }
-                    },
+    let mut y = spring.y;
+
+    while y <= t.bottom_right.y {
+        let mut candidates = vec![];
+
+        for x in t.upper_left.x..=t.bottom_right.x {
+            let p = Vector2::new(x, y);
+            if t.transient(p) && !t.solid(p + D) {
+                candidates.push(p + D)
+            }
+        }
+        let mut backtrack = false;
+        for p in candidates {
+            match (left(t, p), right(t, p)) {
+                (Wall(l), Wall(r)) => {
+                    assert!(l.y == r.y);
+                    for x in (l.x + 1)..r.x {
+                        t.insert(Vector2::new(x, l.y), Type::Water);
+                    }
+                    backtrack = true;
+                }
+                (Hole(l), Wall(r)) => {
+                    for x in (l.x)..r.x {
+                        t.insert(Vector2::new(x, l.y), Type::Transient);
+                    }
+                }
+                (Wall(l), Hole(r)) => {
+                    for x in (l.x + 1)..=r.x {
+                        t.insert(Vector2::new(x, l.y), Type::Transient);
+                    }
+                }
+                (Hole(l), Hole(r)) => {
+                    for x in l.x..=r.x {
+                        t.insert(Vector2::new(x, l.y), Type::Transient);
+                    }
                 }
             }
         }
+        if backtrack {
+            y -= 2;
+        } else {
+            y += 1
+        }
     }
+
+    // while progress {
+    //     let mut todo = vec![p];
+    //     let mut visited = HashSet::new();
+    //     progress = false;
+    //     while let Some(p) = todo.pop() {
+    //         // println!("{:?}", p);
+    //         // print(&t);
+    //         if t.scan.get(&p).is_none() && p.y <= t.bottom_right.y && !visited.contains(&p) {
+    //             visited.insert(p);
+    //             match down(t, p) {
+    //                 None => {}
+    //                 Some(p1) => match (left(t, p1), right(t, p1)) {
+    //                     (Wall(l), Wall(r)) => {
+    //                         // println!("{:?} -> W{:?}W", p, p1);
+    //                         assert!(l.y == r.y);
+    //                         progress = true;
+    //                         for x in (l.x + 1)..r.x {
+    //                             t.scan.insert(Vector2::new(x, l.y), Type::Water);
+    //                         }
+    //                         // todo.push(p)
+    //                     }
+    //                     (Hole(l), Wall(_)) => todo.push(l),
+    //                     (Wall(_), Hole(r)) => {
+    //                         todo.push(r);
+    //                     }
+    //                     (Hole(l), Hole(r)) => {
+    //                         todo.push(l);
+    //                         todo.push(r)
+    //                     }
+    //                 },
+    //             }
+    //         }
+    //     }
+    // }
+}
+fn count_wet(t: &T) -> usize {
+    t.scan
+        .iter()
+        .filter(|&(pos, ty)| {
+            t.upper_left.y <= pos.y
+                && pos.y <= t.bottom_right.y
+                && matches!(ty, Type::Water | Type::Transient)
+        })
+        .count()
 }
 
-fn part1(t: &mut T) -> usize {
-    pour(t, Vector2::new(500, 0));
-    t.scan.values().filter(|t| matches!(t, Type::Water)).count()
+fn count_dry(t: &T) -> usize {
+    t.scan
+        .iter()
+        .filter(|&(pos, ty)| {
+            t.upper_left.y <= pos.y && pos.y <= t.bottom_right.y && matches!(ty, Type::Water)
+        })
+        .count()
 }
 
 pub fn run(filename: &str) {
     let contents = std::fs::read_to_string(filename).unwrap();
     let mut t = parse(&contents);
-    println!("{:?} {:?}", t.upper_left, t.bottom_right);
-    println!("{}", part1(&mut t));
-    print(&t);
+    pour(&mut t, Vector2::new(500, 0));
+    println!("{}", count_wet(&t));
+    println!("{}", count_dry(&t));
+    if false {
+        print(&t)
+    };
 }
 
 #[cfg(test)]
@@ -208,8 +268,7 @@ y=13, x=498..504";
     #[test]
     fn test_example() {
         let mut t = parse(S);
-        let r = part1(&mut t);
-        print(&t);
-        assert_eq!(r, 57)
+        pour(&mut t, Vector2::new(500, 0));
+        assert_eq!(count_wet(&t), 57)
     }
 }
